35a087dff6d7a2ad5db96026af3aee689d4b23d5
mhcgolds <cabelo.maniac@gmail.com>
2017-02-24 11:10:13 -0300

    First commit
    First commit

---
+++ SOQuestionProject/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOQuestionProject.Models
+{
+    public class HomeViewModel
+    {
+        public bool MyBool { get; set; }
+    }
+}
---
+++ SOQuestionProject/Util/CustomHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace System.Web.Mvc
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString CustomCheckBoxFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+        {
+            return new MvcHtmlString(String.Format("{0}{1}",
+                                     html.CheckBoxFor(expression),
+                                     html.DisplayNameFor(expression)));
+        }
+
+        public static MvcHtmlString CustomCheckBoxFor2<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, bool>> expression)
+        {
+            return new MvcHtmlString(String.Format("{0}{1}",
+                                     html.CheckBoxFor(expression),
+                                     html.DisplayNameFor(expression)));
+        }
+    }
+}
